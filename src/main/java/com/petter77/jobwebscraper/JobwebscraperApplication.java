5e90781c4da64d6aa0dde65df534e2ba8c586c85
petter77 <ppawlinski77@gmail.com>
2026-02-08 19:07:28 +0100

    Create basic filestructure

---
+++ src/main/java/com/petter77/jobwebscraper/JobwebscraperApplication.java
@@ -0,0 +1,13 @@
+package com.petter77.jobwebscraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobwebscraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobwebscraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petter77/jobwebscraper/JobwebscraperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petter77.jobwebscraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobwebscraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
