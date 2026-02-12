b208cb9ee23dbb9b7a1c425a52ff2786ebe3090e
petter77 <ppawlinski77@gmail.com>
2026-02-12 22:13:40 +0100

    Add scheduler

--- src/main/java/com/petter77/jobwebscraper/JobwebscraperApplication.java
+++ src/main/java/com/petter77/jobwebscraper/JobwebscraperApplication.java
@@ -2,8 +2,10 @@ package com.petter77.jobwebscraper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JobwebscraperApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/petter77/jobwebscraper/ScrapingScheduler.java
@@ -0,0 +1,35 @@
+package com.petter77.jobwebscraper;
+import org.springframework.stereotype.Component;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class ScrapingScheduler {
+    private final OfferParser offerParser;
+    private final OfferRepository offerRepository;
+
+    public ScrapingScheduler(OfferParser offerParser, OfferRepository offerRepository) {
+        this.offerParser = offerParser;
+        this.offerRepository = offerRepository;
+    }
+
+    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.HOURS)
+    public void scrape() {
+        try{
+            Document doc = Jsoup.connect("https://it.pracuj.pl/praca?et=17%2C1&sc=0&itth=38%2C54%2C41%2C37%2C36%2C34").get();
+
+            Elements offers = doc.select(".tiles_b18pwp01.core_po9665q"); 
+
+            List<Offer> result = offerParser.parseAll(offers); 
+
+            offerRepository.saveAll(result);
+        }
+        catch (Exception e) {
+
+        }
+    }
+}
